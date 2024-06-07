ffd6b1a15a96003eec79f632bd614b71774cb591
krzasa <krzasa96@gmail.com>
2024-06-07 11:30:57 -0400

    added superuser will test if teammates can connect

--- backend_api/admin.py
+++ backend_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
+
+
 # Register your models here.
+from .models import Wood, Lighting,Appliances,Paint,Landscaping
+admin.site.register(Wood)
+admin.site.register(Lighting)
+admin.site.register(Appliances)
+admin.site.register(Paint)
+admin.site.register(Landscaping)
